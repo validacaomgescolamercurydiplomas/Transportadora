cea5436c5e49780afaece0844d7508c0fcb48eb6
ESCOLA TÉCNICA MERCURY <261264649+validacaomgescolamercurydiplomas@users.noreply.github.com>
2026-07-08 08:30:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,72 +2,48 @@
 let clientes = JSON.parse(localStorage.getItem('clientes')) || [];
 
 function salvarCliente(){
+let cliente=document.getElementById('cliente').value;
+let telefone=document.getElementById('telefone').value;
+let cidade=document.getElementById('cidade').value;
+let estado=document.getElementById('estado').value;
 
-let cliente = document.getElementById('cliente').value;
-let telefone = document.getElementById('telefone').value;
-let cidade = document.getElementById('cidade').value;
-let estado = document.getElementById('estado').value;
-
-if(!cliente){
-alert('Informe o nome do cliente');
+if(!cliente || !estado){
+alert('Preencha cliente e estado');
 return;
 }
 
-clientes.push({
-cliente,
-telefone,
-cidade,
-estado
-});
-
-localStorage.setItem('clientes', JSON.stringify(clientes));
-
+clientes.push({cliente,telefone,cidade,estado});
+localStorage.setItem('clientes',JSON.stringify(clientes));
 limpar();
 listarClientes();
-
 }
 
 function listarClientes(){
-
 let tabela=document.getElementById('listaClientes');
-
 tabela.innerHTML='';
 
-clientes.forEach((c,index)=>{
-
-tabela.innerHTML += `
-<tr>
+clientes.forEach((c,i)=>{
+tabela.innerHTML += `<tr>
 <td>${c.cliente}</td>
 <td>${c.telefone}</td>
 <td>${c.cidade}</td>
 <td>${c.estado}</td>
-<td>
-<button onclick="excluirCliente(${index})">Excluir</button>
-</td>
-</tr>
-`;
-
+<td><button onclick="excluirCliente(${i})">Excluir</button></td>
+</tr>`;
 });
-
 }
 
-function excluirCliente(index){
-
-clientes.splice(index,1);
-
+function excluirCliente(i){
+clientes.splice(i,1);
 localStorage.setItem('clientes',JSON.stringify(clientes));
-
 listarClientes();
-
 }
 
 function limpar(){
-
-document.getElementById('cliente').value='';
-document.getElementById('telefone').value='';
-document.getElementById('cidade').value='';
-document.getElementById('estado').value='';
-
+cliente.value='';
+telefone.value='';
+cidade.value='';
+estado.value='';
 }
 
 listarClientes();
