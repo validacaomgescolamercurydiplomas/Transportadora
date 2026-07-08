aac776d4401463f02be235d5b552a191847d1b2b
ESCOLA TÉCNICA MERCURY <261264649+validacaomgescolamercurydiplomas@users.noreply.github.com>
2026-07-08 07:42:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+import Dashboard from './components/Dashboard';
+import Clientes from './components/Clientes';
+import Transportes from './components/Transportes';
+export default function App(){return <main><h1>Controle Transportadora</h1><Dashboard/><Clientes/><Transportes/></main>}
