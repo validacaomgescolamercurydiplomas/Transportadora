92ab8db4a2598dddf693343940c821e1b5bd1f6b
ESCOLA TÉCNICA MERCURY <261264649+validacaomgescolamercurydiplomas@users.noreply.github.com>
2026-07-08 10:15:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,43 @@
-
 let clientes=JSON.parse(localStorage.getItem('clientes'))||[];
 let movimentos=JSON.parse(localStorage.getItem('movimentos'))||[];
 
-function mascaraTelefone(i){
-let v=i.value.replace(/\D/g,'');
-if(v.length>10){
-i.value='('+v.substring(0,2)+') '+v.substring(2,7)+'-'+v.substring(7,11);
-}else if(v.length>6){
-i.value='('+v.substring(0,2)+') '+v.substring(2);
-}
-}
-
 function salvarCliente(){
-if(!nomeCliente.value){alert('Informe o cliente');return}
-clientes.push({
-nome:nomeCliente.value,
-telefone:telefoneCliente.value,
-cidade:cidadeCliente.value,
-estado:estadoCliente.value
-});
+clientes.push({nome:nomeCliente.value,telefone:telefoneCliente.value,cidade:cidadeCliente.value,estado:estadoCliente.value});
 localStorage.setItem('clientes',JSON.stringify(clientes));
-carregarClientes();
+carregar();
 }
 
-function cancelarCliente(){
-nomeCliente.value='';
-telefoneCliente.value='';
-cidadeCliente.value='';
-estadoCliente.value='';
+function excluirCliente(i){
+if(confirm('Excluir este cliente?')){
+clientes.splice(i,1);
+localStorage.setItem('clientes',JSON.stringify(clientes));
+carregar();
+}
 }
 
-function carregarClientes(){
+function carregar(){
 cliente.innerHTML='';
 clientes.forEach(c=>cliente.innerHTML+=`<option>${c.nome}</option>`);
-listarClientes();
-}
-
-function listarClientes(){
-clientesLista.innerHTML='';
-clientes.forEach(c=>{
-clientesLista.innerHTML+=`<tr><td>${c.nome}</td><td>${c.telefone}</td><td>${c.cidade}</td><td>${c.estado}</td></tr>`;
-});
+clientesLista.innerHTML=clientes.map((c,i)=>`<tr><td>${c.nome}</td><td>${c.telefone}</td><td>${c.cidade}</td><td>${c.estado}</td><td><button onclick="excluirCliente(${i})">Excluir</button></td></tr>`).join('');
+mostrar();
 }
 
 function salvarMovimento(){
-if(!data.value){alert('Informe a data');return}
-movimentos.push({
-data:data.value,
-cliente:cliente.value,
-caixas:+caixas.value,
-sacos:+sacos.value,
-valor:+valor.value,
-pago:+pago.value
-});
+movimentos.push({data:data.value,cliente:cliente.value,caixas:+caixas.value,sacos:+sacos.value,valor:+valor.value,pago:+pago.value});
 localStorage.setItem('movimentos',JSON.stringify(movimentos));
 mostrar();
 }
 
-function mostrar(){
-relatorio.innerHTML='';
-movimentos.forEach(x=>{
-relatorio.innerHTML+=`<tr><td>${x.data.split('-').reverse().join('/')}</td><td>${x.cliente}</td><td>${x.caixas}</td><td>${x.sacos}</td><td>${x.caixas+x.sacos}</td><td>R$${x.valor}</td><td>R$${x.pago}</td><td>R$${x.valor-x.pago}</td></tr>`;
-});
+function excluirMovimento(i){
+if(confirm('Excluir esta movimentação?')){
+movimentos.splice(i,1);
+localStorage.setItem('movimentos',JSON.stringify(movimentos));
+mostrar();
+}
 }
 
-carregarClientes();
-mostrar();
+function mostrar(){
+relatorio.innerHTML=movimentos.map((m,i)=>`<tr><td>${m.data}</td><td>${m.cliente}</td><td>${m.caixas}</td><td>${m.sacos}</td><td>${m.caixas+m.sacos}</td><td>R$${m.valor}</td><td>R$${m.pago}</td><td>R$${m.valor-m.pago}</td><td><button onclick="excluirMovimento(${i})">Excluir</button></td></tr>`).join('');
+}
+
+carregar();
