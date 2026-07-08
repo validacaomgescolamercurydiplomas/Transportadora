a6eb93edb7bc062a0a7e73c0cdceddfd65daf0b2
ESCOLA TÉCNICA MERCURY <261264649+validacaomgescolamercurydiplomas@users.noreply.github.com>
2026-07-08 09:46:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,76 @@
+
 let dados=JSON.parse(localStorage.getItem('transportes'))||[];
+let filtrados=dados;
 
 function salvar(){
-let item={
+dados.push({
+data:data.value,
 cliente:cliente.value,
 caixas:Number(caixas.value),
 sacos:Number(sacos.value),
 valor:Number(valor.value),
 pago:Number(pago.value)
-};
-
-dados.push(item);
+});
 localStorage.setItem('transportes',JSON.stringify(dados));
 mostrar();
 }
 
-function mostrar(){
-
-let tabela="";
-let totalCx=0,totalSc=0,totalV=0,totalP=0;
-
+function aplicar(lista){
 let clientes={};
-
-dados.forEach(x=>{
-if(!clientes[x.cliente])
-clientes[x.cliente]={caixas:0,sacos:0,valor:0,pago:0};
-
+lista.forEach(x=>{
+if(!clientes[x.cliente]) clientes[x.cliente]={caixas:0,sacos:0,valor:0,pago:0};
 clientes[x.cliente].caixas+=x.caixas;
 clientes[x.cliente].sacos+=x.sacos;
 clientes[x.cliente].valor+=x.valor;
 clientes[x.cliente].pago+=x.pago;
 });
 
+let html="",tc=0,ts=0,tv=0,tp=0;
+
 Object.keys(clientes).forEach(c=>{
 let x=clientes[c];
-tabela+=`<tr>
-<td>${c}</td>
-<td>${x.caixas}</td>
-<td>${x.sacos}</td>
-<td>${x.caixas+x.sacos}</td>
-<td>R$ ${x.valor}</td>
-<td>R$ ${x.pago}</td>
-<td>R$ ${x.valor-x.pago}</td>
-</tr>`;
-
-totalCx+=x.caixas;
-totalSc+=x.sacos;
-totalV+=x.valor;
-totalP+=x.pago;
+html+=`<tr><td>${c}</td><td>${x.caixas}</td><td>${x.sacos}</td><td>${x.caixas+x.sacos}</td><td>R$ ${x.valor}</td><td>R$ ${x.pago}</td><td>R$ ${x.valor-x.pago}</td></tr>`;
+tc+=x.caixas; ts+=x.sacos; tv+=x.valor; tp+=x.pago;
 });
 
-lista.innerHTML=tabela;
+listaEl.innerHTML=html;
+cx.innerHTML=tc;
+sc.innerHTML=ts;
+it.innerHTML=tc+ts;
+vt.innerHTML="R$ "+tv;
+vp.innerHTML="R$ "+tp;
+vr.innerHTML="R$ "+(tv-tp);
+}
 
-cx.innerHTML=totalCx;
-sc.innerHTML=totalSc;
-it.innerHTML=totalCx+totalSc;
-vt.innerHTML="R$ "+totalV;
-vp.innerHTML="R$ "+totalP;
-vr.innerHTML="R$ "+(totalV-totalP);
+function mostrar(){aplicar(dados)}
+
+function filtrarPeriodo(){
+let a=inicio.value,b=fim.value;
+aplicar(dados.filter(x=>x.data>=a && x.data<=b));
+}
+
+function filtrarHoje(){
+let d=new Date().toISOString().slice(0,10);
+aplicar(dados.filter(x=>x.data==d));
+}
+
+function filtrarSemana(){
+let hoje=new Date();
+let inicioSemana=new Date();
+inicioSemana.setDate(hoje.getDate()-6);
+aplicar(dados.filter(x=>{
+let d=new Date(x.data);
+return d>=inicioSemana && d<=hoje;
+}));
+}
+
+function filtrarMes(){
+let m=new Date().getMonth();
+let a=new Date().getFullYear();
+aplicar(dados.filter(x=>{
+let d=new Date(x.data);
+return d.getMonth()==m && d.getFullYear()==a;
+}));
 }
 
 mostrar();
